fb7f1a7540ebbfb3af46c11f25b0e17d3989de9d
OhMyTOP1999 <610108020023@dpu.ac.th>
2020-09-16 15:30:39 +0700

    food

---
+++ food/data.js
@@ -0,0 +1,58 @@
+const data = [
+    {
+        id:1,
+        name:'ไก่ไม่ยอมนอน',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLDNAATVA2UNJ/hero/cab978976ec14ec3bffc196e49fcd919_1594554201623013383.png',
+        type:'Coupon, Partner, Rice Bowls'
+    },
+    {
+        id:2,
+        name:'Kamu Tea (คามุ ที)',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZC1VN5CBEXGAT/hero/2cac19cd03f84037a8046cd3d9a30b85_1585793335979199515.jpeg',
+        type:'Partner, Bubble Tea'
+    },
+    {
+        id:3,
+        name:'Cafe Amazon (คาเฟ่ อเมซอน)',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTEUGBXT2D2TJ/hero/776901be512444519763199d5fe9a272_1593408148178779119.jpeg',
+        type:'Partner, Coffee & Tea'
+    },
+    {
+        id:4,
+        name:'เก๋อาหารตามสั่ง - ถนนมหาจักร',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZEXHF3KERJFGA/hero/da744dc7c52c481aa383aed9cd16b0b6_1589532114847970839.jpeg',
+        type:'Coupon, Partner, Cooked to Order'
+    },
+    {
+        id:5,
+        name:'ตี๋เย็นตาโฟรสเด็ด - ศาลาว่าการกรุงเทพฯ',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZMTKBDTT2XJAJ/hero/65641d0fcdc14d4a8961f56f0ead6fa5_1598847900883974941.jpeg',
+        type:'Partner, Noodles'
+    },
+    {
+        id:6,
+        name:'Subway (ซับเวย์) - ท่ามหาราช',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZCFHAATNRDJCN/hero/0671a249f48145309f858b0d40a27db4_1600007885589659263.jpeg',
+        type:'Partner, Fast Food'
+    },
+    {
+        id:7,
+        name:'McDonald',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/AWh64KYIZXYdMpch2Zem/list/9ba8781499364093bbf4d2a117ff5cfd_1598194281917869457.jpg',
+        type:'Partner, Burgers, Fast Food, Fried Chicken'
+    },
+    {
+        id:8,
+        name:'KHIANG (เขียง) - ถนนพระอาทิตย์',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLGGETXRBV3A6/hero/253e8b332b804905b721ddf999d8d871_1594631339423966806.jpeg',
+        type:'Partner, Rice Bowls'
+    },
+    {
+        id:9,
+        name:'Potato Corner (โปเตโต้ คอร์เนอร์) - ถนนข้าวสาร',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZLJEVDYC3JFL6/hero/182731a9214e4cb793fc6142442013ba_1600055633637784048.jpeg',
+        type:'Partner, Small Bites/Snacks'
+    }
+]
+
+module.exports = data
